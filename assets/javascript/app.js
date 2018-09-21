88e2f0c69fc869f3b08953454d52753158aaaa6f
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-09-21 18:59:10 -0400

    Working buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,40 @@
+// Make array of items
+var buttonArray = ['fairy', 'dragon', 'troll']
+
+// Function to make initial buttons and append to DOM
+function makeBtn() {
+    for (var i = 0; i < buttonArray.length; i++) {
+        var initialBtn = $('<button>', {id:'gif-button', class:'btn btn-secondary', type:'button'});
+        initialBtn.attr('button-name', buttonArray[i]);
+        initialBtn.text(buttonArray[i]);
+        $('#buttons').append(initialBtn); 
+    }
+}
+
+// Making new buttons from form entry and append to DOM
+$('#btn-form').submit(function(e) {
+    e.preventDefault();
+    var btnName = $('#button-input').val();
+    var newBtn = $('<button>', {id:'gif-button', class:'btn btn-secondary', type:'button'});
+    newBtn.attr('button-name', btnName);
+    newBtn.text(btnName);
+    $('#buttons').append(newBtn);
+});
 
 
-// Pre-populate with initial buttons
+
+
+
+// Document Ready
+$(document).ready(function() {
+    makeBtn();
+});
+
+
+
+
+
+
 
 // Get Ajax URL set up
 
-// Make array of items
