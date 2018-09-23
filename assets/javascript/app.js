c803ab353beb3b9c6ca504bb9a5e4793d6c344b5
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-09-23 12:48:44 -0400

    Updating with some Ajax code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
 // Make array of items
-var buttonArray = ['fairy', 'dragon', 'troll']
+var topics = ['fairy', 'dragon', 'troll']
 
 // Function to make initial buttons and append to DOM
 function makeBtn() {
-    for (var i = 0; i < buttonArray.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
         var initialBtn = $('<button>', {id:'gif-button', class:'btn btn-secondary', type:'button'});
-        initialBtn.attr('button-name', buttonArray[i]);
-        initialBtn.text(buttonArray[i]);
+        initialBtn.attr('button-name', topics[i]);
+        initialBtn.text(topics[i]);
         $('#buttons').append(initialBtn); 
     }
 }
@@ -19,22 +19,40 @@ $('#btn-form').submit(function(e) {
     newBtn.attr('button-name', btnName);
     newBtn.text(btnName);
     $('#buttons').append(newBtn);
+    topics.push(btnName);
 });
 
+ //$('#buttons').html(btnName);
 
+// Seacch Giphy API function and pass it to the DOM
+var searchGiphy = function(btnName) {
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=faries&api_key=HY47GKI5iqOwzCMpbRF5QWlTWx4IbYO3&limit=5';
+    $.ajax({
+        url:queryURL,
+        method:'GET'
+    }).then(function(response) {
+        console.log(response);
+        $('#pictures-div').text(response);
+    });
+};
 
-
+/*$('#-secondary').on('click', function() {
+    var img = $('<img />', {src : });
+    img.appendTo('body');
+}); */
 
 // Document Ready
 $(document).ready(function() {
     makeBtn();
+    searchGiphy();
 });
 
 
-
-
-
-
-
-// Get Ajax URL set up
-
+//Key: HY47GKI5iqOwzCMpbRF5QWlTWx4IbYO3
+ /*var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });*/
+    
+    
+    
+    //console.log(topics);
+    //makeBtn();
